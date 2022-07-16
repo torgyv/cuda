fa51efc826e360977673cff03af7390308409728
torgyv <108770567+torgyv@users.noreply.github.com>
2022-07-16 20:10:22 +0700

    Update and rename al to al.rs

---
+++ al.rs
@@ -0,0 +1,17 @@
+use std::env::current_dir;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let d = current_dir().unwrap();
+
+    let rootfs = d.join("rootfs/");
+    let writedir = d.join("write/");
+
+    std::fs::create_dir_all(&writedir)?;
+
+    let mut child = isolated::Command::new(rootfs, "/bin/sh -c lscpu")
+        .disk_write_to(writedir)
+        .spawn()?;
+
+    child.wait()?;
+    Ok(())
+}
